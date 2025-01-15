6648c472c4660b0ad6316bad845d7b119faf78c2
Egor K <murwoofmur@gmail.com>
2025-01-15 14:30:56 +0700

    hw4 is done

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,12 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void
+  name: string
 };
 
 export const Button = (props: ButtonPropsType) => {
+
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack()
   };
 
   return (
